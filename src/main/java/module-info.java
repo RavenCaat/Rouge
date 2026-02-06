953d2741e2217bbad9e7486a4985e9fa756b0794
RavenCaat <benjoja.hotop@gmail.com>
2026-02-06 09:42:32 +0100

    Gambling

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.rouge.rouge {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.rouge.rouge to javafx.fxml;
+    exports org.rouge.rouge;
+}
---
+++ src/main/java/org/rouge/rouge/HelloController.java
@@ -0,0 +1,14 @@
+package org.rouge.rouge;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
